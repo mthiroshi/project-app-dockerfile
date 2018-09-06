e69faa54a9ee17af756642e16fee50f7796c3b11
Hiroshi MUTO <mutohiroshi0726@gmail.com>
2018-09-06 09:45:38 +0900

    :memo: nginxのコンフファイルをコンテナにコピーしてnginxを自動起動するようにDockerfileを修正

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -2,6 +2,8 @@
 
 # /usr/sbin/nginx -g daemon off;
 # service nginx start;
-# service php7.2-fpm start;
+service php7.2-fpm start;
+ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default;
+cd /var/www/html/project-app-lumen && composer install;
 
-exec "$@"
+exec "$@";
